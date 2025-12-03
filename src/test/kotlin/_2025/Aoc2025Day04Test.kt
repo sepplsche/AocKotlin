e1f7b77e0b41dcfed1ac8390a5ee47a9f0d376ff
Sebastian Bayer <seb.bayer@web.de>
2025-12-03 22:02:35 +0100

    2025 day 4 vorbereitet

---
+++ src/test/kotlin/_2025/Aoc2025Day04Test.kt
@@ -0,0 +1,23 @@
+package _2025
+
+import AocUtils
+import kotlin.test.Test
+
+class Aoc2025Day04Test {
+
+    @Test
+    fun aoc1() {
+        val lines = AocUtils.input(this::class.java)
+        val result = 0
+
+        println("Result: $result")
+    }
+
+    @Test
+    fun aoc2() {
+        val lines = AocUtils.input(this::class.java)
+        val result = 0
+
+        println("Result: $result")
+    }
+}
