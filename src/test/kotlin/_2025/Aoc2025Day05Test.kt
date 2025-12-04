8ae7ecc81551728c89f12585ad6298d47555d1d6
Sebastian Bayer <seb.bayer@web.de>
2025-12-04 20:27:20 +0100

    2025 day 5 prepared

---
+++ src/test/kotlin/_2025/Aoc2025Day05Test.kt
@@ -0,0 +1,22 @@
+package _2025
+
+import AocCoordinates.Point
+import AocUtils
+import kotlin.test.Test
+
+class Aoc2025Day05Test {
+
+    @Test
+    fun aoc1() {
+        val lines = AocUtils.input(this::class.java)
+        val result = 0
+        println("Result: $result")
+    }
+
+    @Test
+    fun aoc2() {
+        val lines = AocUtils.input(this::class.java)
+        val result = 0
+        println("Result: $result")
+    }
+}
