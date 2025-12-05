765a9acfcec643d18ec8aefd8956e8a6597164f4
Sebastian Bayer <seb.bayer@web.de>
2025-12-05 14:28:19 +0100

    2025 day 5 finished

--- src/test/kotlin/_2025/Aoc2025Day05Test.kt
+++ src/test/kotlin/_2025/Aoc2025Day05Test.kt
@@ -1,6 +1,7 @@
 package _2025
 
 import AocUtils
+import kotlin.math.max
 import kotlin.test.Test
 
 class Aoc2025Day05Test {
@@ -28,8 +29,34 @@ class Aoc2025Day05Test {
 
     @Test
     fun aoc2() {
-        AocUtils.input(this::class.java)
-        val result = 0
+        val lines = AocUtils.input(this::class.java)
+
+        val ranges = lines
+            .filter { it.contains("-") }
+            .map { it.split("-") }
+            .map { (it[0].toLong()..it[1].toLong()) }
+            .sortedBy { it.first }
+            .sortedWith { o1, o2 ->
+                if (o1.first != o2.first)
+                    o1.first.compareTo(o2.first)
+                else
+                    o1.last.compareTo(o2.last)
+            }
+
+        val mergedRanges = mutableListOf<LongRange>()
+        var lastRange = ranges[0]
+        for (i in 1..<ranges.size) {
+            val range = ranges[i]
+            if (lastRange.last < range.first) {
+                mergedRanges.add(lastRange)
+                lastRange = range
+                continue
+            }
+            lastRange = (lastRange.first..max(lastRange.last, range.last))
+        }
+        mergedRanges.add(lastRange)
+
+        val result = mergedRanges.sumOf { it.last - it.first + 1 }
         println("Result: $result")
     }
 }
