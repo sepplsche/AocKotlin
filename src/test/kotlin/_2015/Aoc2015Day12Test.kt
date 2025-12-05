29008774586530afd2c0bca82451f724e0766742
Sebastian Bayer <seb.bayer@web.de>
2025-12-05 16:54:42 +0100

    2015 day 12 part 1

--- src/test/kotlin/_2015/Aoc2015Day12Test.kt
+++ src/test/kotlin/_2015/Aoc2015Day12Test.kt
@@ -8,14 +8,57 @@ class Aoc2015Day12Test {
     @Test
     fun aoc1() {
         val lines = AocUtils.input(this::class.java)
+        val line = lines[0]
 
-        println("Result: $lines")
+        val ints = '0'..'9'
+
+        val nums = mutableListOf<Int>()
+        var n = ""
+        line.toList().forEach {
+            if (it == '-') {
+                n = "$it"
+            } else if (ints.contains(it)) {
+                n += "$it"
+            } else if (n.isNotBlank()) {
+                nums.add(n.toInt())
+                n = ""
+            }
+        }
+        val result = nums.sum()
+
+        println("Result: $result")
     }
 
     @Test
     fun aoc2() {
         val lines = AocUtils.input(this::class.java)
+        val line = lines[0]
 
-        println("Result: $lines")
+        val ints = '0'..'9'
+
+        val nums = mutableListOf<Int>()
+        var n = ""
+        var obacht = false
+        line.toList().forEach {
+            if (it == '{') {
+                obacht = true
+            }
+            if (it == '[') {
+                obacht = false
+            }
+
+
+            if (it == '-') {
+                n = "$it"
+            } else if (ints.contains(it)) {
+                n += "$it"
+            } else if (n.isNotBlank()) {
+                nums.add(n.toInt())
+                n = ""
+            }
+        }
+        val result = nums.sum()
+
+        println("Result: $result")
     }
 }
