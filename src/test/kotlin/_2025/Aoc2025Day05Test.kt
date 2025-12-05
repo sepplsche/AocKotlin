31d362a2748a6f7664ec6acfd6fa1cd428932b30
bay <sebastian.bayer@ti8m.ch>
2025-12-05 12:14:25 +0100

    day 5

--- src/test/kotlin/_2025/Aoc2025Day05Test.kt
+++ src/test/kotlin/_2025/Aoc2025Day05Test.kt
@@ -1,6 +1,5 @@
 package _2025
 
-import AocCoordinates.Point
 import AocUtils
 import kotlin.test.Test
 
@@ -9,13 +8,27 @@ class Aoc2025Day05Test {
     @Test
     fun aoc1() {
         val lines = AocUtils.input(this::class.java)
-        val result = 0
+
+        val ranges = lines
+            .filter { it.contains("-") }
+            .map { it.split("-") }
+            .map { (it[0].toLong()..it[1].toLong()) }
+
+        val ids = lines
+            .filter { !it.contains("-") }
+            .filter { !it.isEmpty() }
+            .map { it.toLong() }
+
+        val result = ids
+            .filter { ranges.any { range -> range.contains(it) } }
+            .size
+
         println("Result: $result")
     }
 
     @Test
     fun aoc2() {
-        val lines = AocUtils.input(this::class.java)
+        AocUtils.input(this::class.java)
         val result = 0
         println("Result: $result")
     }
