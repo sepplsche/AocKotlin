19895cc85b6359003c7cdae8faf522f1f4085953
bay <sebastian.bayer@ti8m.ch>
2025-12-08 16:16:47 +0100

    day 7 part 1

--- src/test/kotlin/_2025/Aoc2025Day07Test.kt
+++ src/test/kotlin/_2025/Aoc2025Day07Test.kt
@@ -1,6 +1,5 @@
 package _2025
 
-import AocCoordinates.Point
 import AocUtils
 import kotlin.test.Test
 
@@ -10,45 +9,62 @@ class Aoc2025Day07Test {
     fun aoc1() {
         val lines = AocUtils.input(this::class.java)
 
-        val start = lines[0].indexOf('S')
-
-        val splitters = mutableListOf<Point>()
-        (0..<lines.size).forEach { l ->
-            (0..<lines[l].length).forEach { c ->
-                if (lines[l][c] == '^')
-                    splitters.add(Point.of(l, c))
+        val maxY = lines.size
+        val maxX = lines[0].length
+        val beams = Array(maxY) { CharArray(maxX) }
+        (0..<maxY).forEach { y ->
+            var x = 0
+            while (x < maxX) {
+                if (y > 0 && (lines[y - 1][x] == 'S' || (beams[y - 1][x] == '|') && lines[y][x] != '^')) {
+                    beams[y][x] = '|'
+                } else if (lines[y][x] == '^') {
+                    beams[y][x - 1] = '|'
+                    beams[y][x] = lines[y][x]
+                    beams[y][++x] = '|'
+                } else {
+                    beams[y][x] = lines[y][x]
+                }
+                x++
             }
         }
-        println(splitters)
 
-        var beams = setOf(Point.of(1, start))
-        println(beams)
+        // beams.forEach { println(it) }
 
-        (2..<lines.size).forEach { line ->
-            val newBeams = splitters.filter { it.x == line }
-                .filter { beams.contains(Point.of(it.x - 1, it.y)) }
-                .flatMap {
-                    listOf(Point.of(it.x, it.y - 1), Point.of(it.x, it.y + 1))
-                }.toSet()
-
-            if (newBeams.isEmpty()) {
-                beams = beams.map { Point.of(it.x + 1, it.y) }.toSet()
-            } else {
-                beams = newBeams
+        var counter = 0
+        (0..<maxY).forEach { y ->
+            (0..<maxX).forEach { x ->
+                if (beams[y][x] == '^' && beams[y - 1][x] == '|') {
+                    counter++
+                }
             }
-
-            println(beams)
         }
 
-        val result = 0
-
-        println("Result: $result")
+        println(counter)
     }
 
     @Test
     fun aoc2() {
         val lines = AocUtils.input(this::class.java)
-        val result = lines
-        println("Result: $result")
+
+        val maxY = lines.size
+        val maxX = lines[0].length
+        val beams = Array(maxY) { CharArray(maxX) }
+        (0..<maxY).forEach { y ->
+            var x = 0
+            while (x < maxX) {
+                if (y > 0 && (lines[y - 1][x] == 'S' || (beams[y - 1][x] == '|') && lines[y][x] != '^')) {
+                    beams[y][x] = '|'
+                } else if (lines[y][x] == '^') {
+                    beams[y][x - 1] = '|'
+                    beams[y][x] = lines[y][x]
+                    beams[y][++x] = '|'
+                } else {
+                    beams[y][x] = lines[y][x]
+                }
+                x++
+            }
+        }
+
+        beams.forEach { println(it) }
     }
 }
