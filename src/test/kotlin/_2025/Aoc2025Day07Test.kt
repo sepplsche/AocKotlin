c11936503558d276a29d46038fc4c4ef43ea87b5
bay <sebastian.bayer@ti8m.ch>
2025-12-09 14:32:13 +0100

    day 7 part 2 prepared

--- src/test/kotlin/_2025/Aoc2025Day07Test.kt
+++ src/test/kotlin/_2025/Aoc2025Day07Test.kt
@@ -48,23 +48,25 @@ class Aoc2025Day07Test {
 
         val maxY = lines.size
         val maxX = lines[0].length
-        val beams = Array(maxY) { CharArray(maxX) }
+        val beams = Array(maxY) { IntArray(maxX) }
+        beams[0][lines[0].indexOf('S')] = 1
+
         (0..<maxY).forEach { y ->
             var x = 0
             while (x < maxX) {
-                if (y > 0 && (lines[y - 1][x] == 'S' || (beams[y - 1][x] == '|') && lines[y][x] != '^')) {
-                    beams[y][x] = '|'
+                if (y > 0 && (beams[y - 1][x] > 0) && lines[y][x] != '^') {
+                    beams[y][x] = beams[y - 1][x]
                 } else if (lines[y][x] == '^') {
-                    beams[y][x - 1] = '|'
-                    beams[y][x] = lines[y][x]
-                    beams[y][++x] = '|'
-                } else {
-                    beams[y][x] = lines[y][x]
+                    beams[y][x - 1] += beams[y - 1][x] + beams[y - 1][x - 1]
+                    beams[y][x] = 0
+                    beams[y][++x] += beams[y - 1][x - 1] + beams[y - 1][x]
+                } else if (y > 0) {
+                    beams[y][x] = 0
                 }
                 x++
             }
         }
 
-        beams.forEach { println(it) }
+        beams.forEach { line -> println(line.toList().map { if (it == 0) "  " else it.toString().padStart(2) }) }
     }
 }
